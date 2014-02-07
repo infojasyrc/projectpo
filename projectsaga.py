5c8c2c059837f3d154d94d2942bfd4faca5a010d
infojasyrc <infojasyrc@gmail.com>
2014-02-07 15:15:33 -0500

    Add a file for sending alert in a network on Windows

--- projectsaga.py
+++ projectsaga.py
@@ -16,20 +16,29 @@ class MyHandler(PatternMatchingEventHandler):
     patterns=["*.PO", "*.po"]
 
     def process(self, event):
+        user_to_notify = "Oscar"
         path = os.path.dirname(__file__)
         command = "projectsaga"
-        launcher = os.path.join(path, command)
+        command_alert = "enviar_mensaje"
         
-        command = '%s "%s"' % (launcher, event.src_path)
+        launcher = os.path.join(path, command)
+        launcher_alert = os.path.join(path, command_alert)
+        
+        full_command = '%s "%s"' % (launcher, event.src_path)
         
         try: 
-            # Execute command to mount folder
-            p = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+            # Execute command
+            p = Popen(full_command, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
             # Get output
             rs_cmd = p.stdout.read()
             
             if rs_cmd=="":
-                print "Archivo registrado exitosamente: %s" % event.src_path
+                content_alert = "Archivo registrado exitosamente: %s" % event.src_path
+                # print content_alert
+                full_command_alert = '%s "%s" "%s"' % (launcher_alert, user_to_notify, content_alert)
+                
+                Popen(full_command_alert, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+                
             else:
                 print "Error al ingresar el archivo: %s" % event.src_path
             
